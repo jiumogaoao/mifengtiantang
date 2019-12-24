8374f1d371ba267b072b9d68af0d08acc74f6696
jiumogaoao <394127821@qq.com>
2019-12-24 16:07:25 +0800

    1
    1

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -7,11 +7,11 @@
 				<view class="headFrame">
 					<image src="/static/logo@2x.png"></image>
 					<view class="navFrame">
-						<view class="nav" @click="scrollTo(0)">首页<view class="line" v-if="scrollTopNow>=0 && scrollTopNow<899"></view></view>
-						<view class="nav" @click="scrollTo(899)">关于蜜蜂天堂<view class="line" v-if="scrollTopNow>=899 && scrollTopNow<1344"></view></view>
-						<view class="nav" @click="scrollTo(1344)">简介<view class="line" v-if="scrollTopNow>=1344 && scrollTopNow<3266"></view></view>
-						<view class="nav" @click="scrollTo(3266)">下载<view class="line" v-if="scrollTopNow>=3266 && scrollTopNow<3622"></view></view>
-						<view class="nav" @click="scrollTo(3622)">联系我们<view class="line" v-if="scrollTopNow>=3622"></view></view>
+						<view :class="{nav:1,hl:scrollTopNow>=0 && scrollTopNow<899}" @click="scrollTo(0)">首页<view class="line" v-if="scrollTopNow>=0 && scrollTopNow<899"></view></view>
+						<view :class="{nav:1,hl:scrollTopNow>=899 && scrollTopNow<1344}" @click="scrollTo(899)">关于蜜蜂天堂<view class="line" v-if="scrollTopNow>=899 && scrollTopNow<1344"></view></view>
+						<view :class="{nav:1,hl:scrollTopNow>=1344 && scrollTopNow<3266}" @click="scrollTo(1344)">简介<view class="line" v-if="scrollTopNow>=1344 && scrollTopNow<3266"></view></view>
+						<view :class="{nav:1,hl:scrollTopNow>=3266 && scrollTopNow<3622}" @click="scrollTo(3266)">下载<view class="line" v-if="scrollTopNow>=3266 && scrollTopNow<3622"></view></view>
+						<view :class="{nav:1,hl:scrollTopNow>=3622}" @click="scrollTo(3622)">联系我们<view class="line" v-if="scrollTopNow>=3622"></view></view>
 					</view>
 				</view>
 			</view>
@@ -19,14 +19,23 @@
 					<view :class="{frameL:1,frame1:1,canShow:indexShow}">
 										<view class="frameS">
 											<view class="left">
-												<view class="titleL">蜜蜂天堂最新版</view>
-												<view class="titleL">汇聚主流平台优惠券</view>
-												<view class="titleL">让你不仅省钱，还赚钱</view>
-												<image class="qrCode" src="/static/qrcode@2x2.png"></image>
-												<view class="titleL">每天都是双十一</view>
-												<view class="titleL">扫码下载安装</view>
-												<view class="titleL">邀请码：224422</view>
-												<view class="text">感受极致的购物体验,注册下载后记得加</view>
+												<view class="titleL">蜜蜂天堂</view>
+												<view class="titleL">自用省钱，分享赚钱</view>
+												<view class="titleL">0成本，轻创业，无负担</view>
+												<view class="qrFrame">
+													<view class="qrPoint">
+														<view class="qrTitle">安卓版</view>
+														<image src="/static/AOSQRcode.png"></image>
+													</view>
+													<view class="qrPoint">
+														<view class="qrTitle">苹果版</view>
+														<image src="/static/IOSQRcode.png"></image>
+													</view>
+												</view>
+												<view class="titleL">快效激活你的粉丝经济</view>
+												<view class="titleL">一个能帮消费者省钱赚钱的APP</view>
+												<!-- <view class="titleL">邀请码：224422</view>
+												<view class="text">感受极致的购物体验,注册下载后记得加</view> -->
 												<view class="buttonFrame">
 													<view class="button" style="background-color: #EC5298;" @click="scrollTo(3266)">高速通道下载</view>
 													<view class="button" style="background-color: #FBB03B;" @click="scrollTo(3266)">下载APP</view>
@@ -38,40 +47,34 @@
 									<view :class="{frameL:1,frame2:1,canShow:canShow(100+800+100)}">
 										<view class="frameM frameM1">
 											<image class="picS" src="/static/huojianICON@2x.png"></image>
-											<view class="titleM">蜜蜂天堂APP的诞生</view>
-											<view class="text">2019年3月项目策划团队筹建，2019年11月测试版APP上线，2019年12月正式版上线，我们将不断优化用户的购物体验，为大家创造额外的被动可持续收入，资产永续，不再是一个名词！而是拿得到的收益！</view>
+											<view class="titleM">帮省加油费</view>
+											<view class="text">点击寻找距离最近、最省钱的加油站！</view>
 										</view>
 										<view class="frameM frameM2">
 											<image class="picS" src="/static/huiyuanICON@2x.png"></image>
-											<view class="titleM">蜜蜂天堂的优势</view>
-											<view class="text">蜜蜂天堂消费省钱、轻松赚钱、享受增值。查询电商平台所有优惠，每天多达上万种品类商品，让你每一分的消费都有所价值！ 解决传统积分的“散，乱，价值低”等问题，海量精美优质的积分兑换商品供你挑选！区块链云算力的增值服务为你资产保驾护航，助你实现资产永续！更多惊喜等你体验！ 创新思维 颠覆传统</view>
+											<view class="titleM">帮赚奶粉钱</view>
+											<view class="text">分享APP给身边亲友，获得蜜糖奖励！</view>
 										</view>
 										<view class="frameM frameM3">
 											<image class="picS" src="/static/jifengICON@2x.png"></image>
-											<view class="titleM">蜜蜂天堂的未来</view>
-											<view class="text">蜜蜂天堂以用户体验为重点，打造社交新零售生态系统，让实体店商家、消费者、线上店铺等，一起实现多方共赢，为大家创造价值，欢迎宝妈、微商，线上线下商家、淘客加入我们的大家庭！共创美好的明天！</view>
+											<view class="titleM">帮淘优惠券</view>
+											<view class="text">海量淘宝平台隐藏优惠券，同样是花钱，这里更享经济实惠！</view>
 										</view>
 									</view>
 									<view :class="{frameL:1,frame3:1,canShow:canShow(100+800+440+100)} ">
 										<view class="frameS">
 											<view class="left">
 												<view class="titleL">蜜蜂天堂注册流程</view>
-												<view class="text">蜜蜂天堂通过邀请注册，扫码确定关系终身不变，比亲妈还亲。</view>
-												<view class="point">
-													<image class="picXS" src="/static/wangyeICON@2x.png"></image>
-													<view class="text">点击网页下载链接，下载注册蜜蜂天堂，填写邀请码224422</view>
-												</view>
-												<view class="point">
-													<image class="picXS" src="/static/bibianjiICON@2x.png"></image>
-													<view class="text">登陆后.扫码添加微信客服，或者加蜜蜂天堂APP商城回复手机后四位</view>
-												</view>
-												<view class="point">
-													<image class="picXS" src="/static/jingbaoICON@2x.png"></image>
-													<view class="text">小贴士：注册手机号也可以作为邀请码哦。</view>
-												</view>
-												<view class="buttonFrame">
-													<view class="button" style="background-color: #786DE3;" @click="scrollTo(3266)">高速通道下载</view>
-													<!-- <view class="button" style="background-color: #212121;">更多</view> -->
+												<view class="text">扫描二维码注册，下载！</view>
+												<view class="qrFrame">
+													<view class="qrPoint">
+														<view class="qrTitle">安卓版</view>
+														<image src="/static/AOSQRcode.png"></image>
+													</view>
+													<view class="qrPoint">
+														<view class="qrTitle">苹果版</view>
+														<image src="/static/IOSQRcode.png"></image>
+													</view>
 												</view>
 											</view>
 											<image class="picL" src="/static/home@2x.png"></image>
@@ -81,10 +84,11 @@
 										<view class="frameS">
 											<image class="picL" src="/static/home@2x.png"></image>
 											<view class="left">
-												<view class="titleL">蜜蜂天堂核心优势</view>
-												<view class="text">APP底部商学院栏目打造自学乐园，底部社区有各种素材可以分享， 零门槛零风险：真正的0投资、0成本、0囤货、0发货； 全领域全覆盖：合作伙伴几乎涵盖所有领域知名电商； 超高购物返佣：秒杀同类平台，百万大额优惠券每日更新；</view>
-												<view class="text">对接主流电商平台商品，花生日记可以搜索你想找的商品。觉得不过瘾？复制宝贝标题搜索试试</view>
-												<view class="text">使用搜索也有小技巧，觉得搜冰箱搜不出来，只能在淘宝APP复制标题反向查找？搜索电冰箱试试，嘿嘿，我们将在不断地版本更新中进行优化的亲</view>
+												<view class="titleL">蜜蜂天堂APP功能概述</view>
+												<view class="text">据淘宝数据统计，平均每人淘宝购物一年消费3万左右，通过蜜蜂天堂领取优惠省钱，每年一个家庭网购支出将节省40%至61%。</view>
+												<view class="text">蜜蜂天堂，能帮粉丝、用户、消费者省钱的APP！</view>
+												<view class="text">这里筛选了海量电商平台让利超狠的商品，惊爆眼球的大额优惠券和促销价环环相“扣”，一样是花钱，这里更享经济优惠！</view>
+												<view class="text">每次成功消费额外赚到蜜糖积分，每一蜜糖都能兑换平台优选的自营产品，均可最高抵扣100%，优选产品免费拿！</view>
 												<view class="buttonFrame">
 													<view class="button" style="background-color: #10CFBD;" @click="scrollTo(3266)">高速通道下载</view>
 													<!-- <view class="button" style="background-color: #EC5298;">更多</view> -->
@@ -92,29 +96,49 @@
 											</view>
 										</view>
 									</view>
-									<view :class="{frameL:1,frame3:1,frame5:1,canShow:canShow(100+800+440+640+640+100)}">
+									<view :class="{frameL:1,frame3:1,frame8:1,canShow:canShow(100+800+440+640+640+100)}">
 										<view class="frameS">
 											<view class="left">
-												<view class="titleL">如何分享赚钱</view>
-												<view class="text">在APP端，右下角我-分享好友中，我们有多重推广方式，以推广APP为主，用你的邀请码或者邀请链接下载注册的自动成为你团队小伙伴哦。当然，你也可以分享商品口令推荐你所喜欢的商品</view>
-												<view class="point">
-													<image class="picXS" src="/static/shoujiICON@2x2.png"></image>
-													<view class="text">APP邀请推广</view>
+												<view class="titleL">什么是蜜糖？</view>
+												<view class="list">
+													<view class="li">
+														蜜糖的来源
+													</view>
+													<view class="li">
+														蜜糖来源于平台合作的商家的佣金，在刨除技术研发、运营服务等硬性成本后，平台将大部分佣金折算为蜜糖积分。
+													</view>
+													<view class="li">
+														蜜糖的归属
+													</view>
+													<view class="li">
+														蜜糖用于奖励在平台消费的用户以及平台发展做出杰出贡献的引荐人。
+													</view>
+													<view class="li">
+														蜜糖的用途
+													</view>
+													<view class="li">
+														蜜糖可用于购买自营商城、本地生活的产品和服务（2.0版本正式上线）。
+													</view>
 												</view>
-												<view class="point">
-													<image class="picXS" src="/static/shangpinICON@2x.png"></image>
-													<view class="text">商品分享推广</view>
+												<view class="buttonFrame">
+													<view class="button" style="background-color: #EC5298;" @click="scrollTo(3266)">高速通道下载</view>
+													<!-- <view class="button" style="background-color: #212121;">更多</view> -->
 												</view>
-												<view class="point">
-													<image class="picXS" src="/static/fenxiangICON@2x.png"></image>
-													<view class="text">多种分享方式</view>
-												</view>
+											</view>
+											<image class="picL" src="/static/home@2x.png"></image>
+										</view>
+									</view>
+									<view :class="{frameL:1,frame3:1,frame5:1,canShow:canShow(100+800+440+640+640+250+100)}">
+										<view class="frameS">
+											<image class="picL" src="/static/home@2x.png"></image>
+											<view class="left">
+												<view class="titleL">如何分享赚钱创业？</view>
+												<view class="text">点击邀请生成专属二维码，成为引荐人！ 分享给好友注册下载！ 好友通过平台每一笔消费都能给您带来蜜糖收益！</view>
 												<view class="buttonFrame">
 													<view class="button" style="background-color: #786DE3;" @click="scrollTo(3266)">高速通道下载</view>
 													<!-- <view class="button" style="background-color: #212121;">更多</view> -->
 												</view>
 											</view>
-											<image class="picL" src="/static/home@2x.png"></image>
 										</view>
 									</view>
 									<view :class="{frameL:1,frame6:1,canShow:(100+800+440+640+640+640+100)}">
@@ -122,9 +146,9 @@
 										<view class="text">高速下载通道下载请点击</view>
 										<view class="buttonFrame">
 											<view class="button" style="background-color: #EC5298;" @click="downloadAOS">
-												<image class="icon" src="/static/ANZHUO@2x.png"></image><text>安卓 GOOGLE PLAY</text>
+												<text>安卓 GOOGLE PLAY</text>
 											</view>
-											<view class="button" style="background-color: #FBB03B;" @click="download"><image class="icon" src="/static/IOS@2x.png"></image><text>苹果 APP STORE</text></view>
+											<view class="button" style="background-color: #FBB03B;" @click="download"><text>苹果 APP STORE</text></view>
 										</view>
 									</view>
 									<view :class="{frameL:1,frame7:1,canShow:canShow(100+800+440+640+640+640+360+100)} ">
@@ -180,7 +204,16 @@
 												<view class="titleL">蜜蜂天堂最新版</view>
 												<view class="titleL">汇聚主流平台优惠券</view>
 												<view class="titleL">让你不仅省钱，还赚钱</view>
-												<image class="qrCode" src="/static/qrcode@2x2.png"></image>
+												<view class="qrFrame">
+													<view class="qrPoint">
+														<view class="qrTitle">安卓版</view>
+														<image src="/static/AOSQRcode.png"></image>
+													</view>
+													<view class="qrPoint">
+														<view class="qrTitle">苹果版</view>
+														<image src="/static/IOSQRcode.png"></image>
+													</view>
+												</view>
 												<view class="titleL">每天都是双十一</view>
 												<view class="titleL">扫码下载安装</view>
 												<view class="titleL">邀请码：224422</view>
@@ -505,6 +538,12 @@
 								margin: auto;
 								margin-top: 11px;
 							}
+							&.hl{
+								color: #FAC523;
+								.line{
+									background-color: #FAC523;
+								}
+							}
 						}
 					}
 				}
@@ -517,6 +556,25 @@
 				overflow-x: hidden;
 				.frameL{
 					width: 100%;
+					.qrFrame{
+						margin-top: 27px;
+						margin-bottom: 27rpx;
+						display: flex;
+						.qrPoint{
+							margin-right: 25px;
+							.qrTitle{
+								font-size:16px;
+								font-family:Microsoft YaHei;
+								font-weight:bold;
+								color:rgba(255,255,255,1);
+							}
+							image{
+								width: 180px;
+								height:180px;
+								margin-top: 10rpx;
+							}
+						}
+					}
 					.titleL{
 						font-size:30px;
 						font-family:Microsoft YaHei;
@@ -558,7 +616,7 @@
 					}
 				}
 				.frame1{
-					background-color: rgba(120,109,227,1);
+					background-color: #E36D6D;
 					height: 804px;
 					.left{
 						margin-top: 104px;
@@ -609,6 +667,7 @@
 					height: 440px;
 					.frameM{
 						flex-shrink: 1;
+						width: 33.335%;
 						padding: 72px 27px;
 						.picS{
 							transition-duration: 0.6s;
@@ -638,13 +697,14 @@
 							color: #F4F5F9;
 							margin-top: 20px;
 							opacity: 0;
+							text-align: center;
 						}
 					}
 					.frameM1{
 						background-color: #212121;
 					}
 					.frameM2{
-						background-color: #EC5298;
+						background-color: #10CFBD;
 					}
 					.frameM3{
 						background-color: #F4F5F9;
@@ -680,9 +740,15 @@
 							transition-timing-function: linear;
 							transform:translate(100px,0);
 							color:#666666;
-							margin-top: 29px;
-							margin-bottom: 53px;
+							margin-top: 30px;
+							// margin-bottom: 9px;
 							opacity: 0;
+						}
+						.qrTitle{
+							font-size:16px;
+							font-family:Microsoft YaHei;
+							font-weight:400;
+							color:rgba(102,102,102,1) !important;
 						}
 						.point{
 							display: flex;
@@ -726,7 +792,7 @@
 				}
 				.frame4{
 					height: 640px;
-					background-color: #FBB03B;
+					background-color: #EC5298;
 					.picL{
 						transition-duration: 0.6s;
 						transition-timing-function: linear;
@@ -779,7 +845,7 @@
 				}
 				.frame6{
 					height: 360px;
-					background-color: #786DE4;
+					background-color: #6D94E4;
 					.titleL{
 						transition-duration: 0.6s;
 						transition-timing-function: linear;
@@ -917,6 +983,34 @@
 						}
 					}
 				}
+				.frame8{
+					// <view class="list">
+					// 	<view class="li">
+					// 		<view class="dot"></view>
+					// 		<text>蜜糖的来源</text>
+					// 	</view>
+					background-color: #FAC523;
+					&.frame3{
+						.left{
+							.titleL{
+								color: #fff !important;
+							}
+						}
+					}
+					.list{
+						margin-top: 30px;
+						.li{
+							width: 540px;
+							display:list-item;
+							list-style-type:disc;
+							font-size:14px;
+							font-family:Microsoft YaHei;
+							font-weight:400;
+							color:rgba(244,245,249,1);
+							line-height:24px;
+						}
+					}
+				}
 				.frameL.canShow{
 					.picL,.button,.label,.title,.icon,.text,.titleL,.picXS,.titleM,.picXS,.picS,.qrCode,.qrCode2,.subTitle{
 						transform:translate(0,0) rotate(0deg) scale(1,1) !important;
@@ -975,6 +1069,12 @@
 					color:rgba(187,182,241,1);
 					margin-top: 60rpx;
 					position: relative;
+					&.hl{
+						color: #FAC523;
+						.line{
+							background-color: #FAC523;
+						}
+					}
 				}
 			}
 		}
@@ -994,6 +1094,24 @@
 				.frameL{
 					width: 750rpx;
 					padding: 60rpx 30rpx;
+				}
+				.qrFrame{
+					margin-top: 11rpx;
+					display: flex;
+					.qrPoint{
+						margin-right: 10rpx;
+						.qrTitle{
+							font-size:6rpx;
+							font-family:Microsoft YaHei;
+							font-weight:bold;
+							color:rgba(255,255,255,1);
+						}
+						image{
+							width: 70rpx;
+							height:70rpx;
+							margin-top: 4rpx;
+						}
+					}
 				}
 				.titleL{
 					transition-duration: 0.6s;
@@ -1056,14 +1174,14 @@
 					transform:translate(0,0) rotate(90deg) scale(1.5,1.5);
 				}
 				.frame1{
-					background-color: rgba(120,109,227,1);
-					.qrCode{
-						width: 354rpx;
-						height: 346rpx;
-						margin: auto;
-						margin-top: 30rpx;
-						margin-bottom: 38rpx;
-					}
+					background-color: #E36D6D;
+					// .qrCode{
+					// 	width: 354rpx;
+					// 	height: 346rpx;
+					// 	margin: auto;
+					// 	margin-top: 30rpx;
+					// 	margin-bottom: 38rpx;
+					// }
 					.left{
 						.text{
 							color: #fff;
@@ -1103,10 +1221,10 @@
 						background-color: #212121;
 					}
 					.frameM2{
-						background-color: #EC5298;
+						background-color: #10CFBD;
 					}
 					.frameM3{
-						background-color: #10CFBD;
+						background-color: #F4F5F9;
 					}
 				}
 				.frame3{
@@ -1144,7 +1262,7 @@
 						}
 				}
 				.frame4{
-					background-color: #FBB03B;
+					background-color: #EC5298;
 					.titleL{
 						margin-top: 50rpx;
 						margin-bottom: 50rpx;
@@ -1178,7 +1296,7 @@
 					}
 				}
 				.frame6{
-					background-color: #786DE4;
+					background-color: #6D94E4;
 					.titleL{
 						color: #fff;
 						margin-bottom: 50rpx;
@@ -1230,6 +1348,9 @@
 							text-align: center;
 						}
 					}
+				}
+				.frame8{
+					background-color: #FAC523;
 				}
 				.foot{
 					padding: 60rpx 30rpx;
